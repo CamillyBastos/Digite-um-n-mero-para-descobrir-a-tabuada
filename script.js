9400de17a83a920f22938bd7b088df6cc63fe470
Camilly Bastos <camillyteixeirabastoslopes@gmail.com>
2025-04-08 19:26:28 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+var res = document.querySelector(".res")
+var numero = Number(prompt("Digite um número para descobrir a tabuada"))
+
+for(var inicio =1; inicio <10; inicio++){
+alert(inicio)
+res.innerHTML += `<p>${numero} x ${inicio} 
+= ${numero * inicio}</p>`
+
+}
